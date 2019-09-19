ca9551a9c58ed32bdb7373014800dd2a4a4a26c1
zabalacj0762 <55559313+zabalacj0762@users.noreply.github.com>
2019-09-19 16:28:39 -0400

    Add files via upload
    Second submission

---
+++ P2T1_SalesPrediction_JUANZABALACANDO.py
@@ -0,0 +1,10 @@
+#get th projected total sales
+#17 September 2019
+#CTI-110-P2T1
+#JUAN ZABALA CANDO
+#
+total_sales= float(input('Enter the projected sales: '))
+#calculate the profit as 23 percent of total sales
+profit= total_sales *0.23
+#display the profit
+print('the profit is $', format(profit, ',.2f'))
